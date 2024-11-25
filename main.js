37f4f9b5ebad5a44ebeeec553f0fdbaae9d8b21a
ABLEOFFICIAL <ekwesijoshua@gmail.com>
2024-11-25 21:09:52 +0100

    api learning

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let btn = document.getElementById('btn');
+let about = document.getElementById('about');
 
 
 btn.addEventListener('click', function(){
@@ -7,4 +8,28 @@ btn.addEventListener('click', function(){
     .then((data) => {
         document.getElementById('text-space').innerHTML = data;
     });
+})
+
+about.addEventListener('click', function(){
+    fetch('aboutMe.txt')
+    .then((res) => res.text())
+    .then((data) => {
+        let about = document.getElementById('about-space');
+        about.innerHTML = data;
+    })
+})
+
+document.getElementById('post').addEventListener('click', function(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((res) => res.json())
+    .then((data) => {
+        let post = document.getElementById('post-space');
+        post.innerHTML = '<h3>Users</h3>';
+        data.forEach((item) => {
+            post.innerHTML += `<p>${item.title}</p>`
+            post.innerHTML += `<p>${item.userId}</p>`
+            post.innerHTML += `<p>${item.body}</p>`
+
+        })
+    })
 })
